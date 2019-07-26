ca890c132155103b2e79002d0652b2a6ea463157
cuetod <52927694+cuetod@users.noreply.github.com>
2019-07-26 10:17:44 -0400

    Add files via upload

---
+++ assets/Data/myBooks.ts
@@ -0,0 +1,29 @@
+import { Books } from '../../app/books';
+
+export const MYBOOKS: Books[] = 
+[
+    {
+        bookName: "The Fault in Our Stars", 
+        author: "John Green", 
+        genre: "Romance", 
+        pic: 'tfios.jpg'
+    },
+    {
+        bookName: "Divergent", 
+        author: "Veronica Roth", 
+        genre: "Science Fiction",
+        pic: 'div.jpg' 
+    },
+    {
+        bookName: "City of Bones", 
+        author: "Cassandra Clare", 
+        genre: "Urban fantasy", 
+        pic: 'cob.jpg' 
+    },
+    {
+        bookName: "The Maze Runner", 
+        author: "James Dashner", 
+        genre: "Science Fiction", 
+        pic:'mr.jpg' 
+    }
+]
